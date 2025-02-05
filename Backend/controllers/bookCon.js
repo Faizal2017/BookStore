5ee7370ab099a76f8e71baa1f5079e19aaaaf28f
Faizal Amaan <123483157+Faizal2017@users.noreply.github.com>
2025-02-05 11:19:14 +0530

    controller class updatwd

--- Backend/controllers/bookCon.js
+++ Backend/controllers/bookCon.js
@@ -1,6 +1,8 @@
 import { response } from "express";
 import { Book } from "../models/Books.js";
 
+
+// function to create a book
 export const CreateBook = async (request ,response)=>{
 try{
     if(
@@ -25,5 +27,92 @@ catch(error){
     console.log(error.message)
     return response.send(400).send({message:error.message})
 }
+}
 
+
+// function to get all books
+export const getAllBooks = async (request,response)=>{
+ 
+     try{
+        const books = await Book.find({})
+        return response.status(200).json(books)
+
+     }
+     catch(error){
+        console.log(error.message)
+        return response.status(404).send({message: error.message})
+
+     }
+
+}
+
+
+
+// function to a book By ID
+export const FindBookById = async (request,response)=>{
+ 
+    try{
+        const {id}= request.params;
+
+       const book = await Book.findById(id);
+       return response.status(200).send(book);
+
+    }
+    catch(error){
+       console.log(error.message)
+       return response.status(404).send({message: error.message})
+
+    }
+
+}
+
+
+//function to update a book
+export const updateBook = async (request , response)=>{
+
+    try{
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishyear
+        ){
+            return response.status(400).send({ message: "send all required data"})
+        }
+
+        const {id}= request.params;
+
+        const result = await Book.findByIdAndUpdate(id,request.body);
+        if(!result){
+            return response.status(404).json({message: 'book not found'});
+        }
+        
+        return response.status(404).send({message:'Book Updated Successfully'})
+
+
+
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(400).send({message:error.message})
+    }
+
+
+}
+
+//function to delete a book
+export const deleteBook =  async (request,response)=>{
+    try{
+        const {id}= request.params;
+        const result = await Book.findByIdAndDelete(id);
+
+        if(!result){
+            return response.status(404).json({message:'Book not found'});
+
+        }
+        return response.status(202).send({message:'Deleted successfully'});
+
+    }catch(error){
+        console.log(error.message);
+       return response.status(404).send({message:error.message});
+    }
 }
